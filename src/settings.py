fec874e69243970c9773f0e64c2b62bf6e0492e7
Avazbek <hazratovavazbek994104879@gmail.com>
2023-07-11 17:13:26 +0500

    gg

--- src/settings.py
+++ src/settings.py
@@ -31,13 +31,13 @@ AUTH_USER_MODEL = 'materio.user'
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'rest_framework.authtoken',
+    'rest_framework.authtoken',
 
     'materio',
 ]
