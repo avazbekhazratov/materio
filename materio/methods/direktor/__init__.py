43e89d7e061e6f1485d2552d1299c19d00cedbb0
Avazbek <hazratovavazbek994104879@gmail.com>
2023-07-11 19:26:33 +0500

    Update __init__.py

--- materio/methods/direktor/__init__.py
+++ materio/methods/direktor/__init__.py
@@ -1,2 +1,3 @@
 from materio.methods.direktor.xodimlar import add_xodim, get_xodim, update_xodim
 from .product import get_product, add_product, update_product, delete_product
+from .home_page import user_deletes
